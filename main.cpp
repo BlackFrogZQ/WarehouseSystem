5379bfe89e6b3c73f79e7a636f46f03642d89e94
BlackFrogZQ <1275448553@qq.com>
2023-02-12 14:46:24 +0800

    防止软件重复运行

--- main.cpp
+++ main.cpp
@@ -1,39 +1,25 @@
 ﻿#include "src/ui/mainWindow.h"
-#include <QApplication>
-#include <QMessageBox>
-#include <QDir>
-#include <QSettings>
+#include <QIcon>
+#include <QtSingleApplication>
 #pragma comment(lib, "user32.lib")
 void initSystemService();
 void closeSystemService();
 
-#define AUTO_RUN_KEY	"HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run"
-void setMyAppAutoRun(bool isStart)
-{
-    QString application_name = QApplication::applicationName();
-    QSettings *settings = new QSettings(AUTO_RUN_KEY, QSettings::NativeFormat);
-    if(isStart)
-    {
-        QString application_path = QApplication::applicationFilePath();
-        settings->setValue(application_name, application_path.replace("/", "\\"));
-    }
-    else
-    {
-        settings->remove(application_name);
-    }
-}
-
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    setMyAppAutoRun(true);
-    a.setWindowIcon(QIcon(":res/storage.png"));
+    QtSingleApplication instance(argc, argv);
+    if (instance.sendMessage("Wake up!"))
+    {
+        return 0;
+    }
+    instance.setWindowIcon(QIcon(":res/storage.png"));
     initSystemService();
     CMainWindow w;
     w.setWindowTitle("StorageSystem");
     // w.show();
     w.showFullScreen();
-    int code = a.exec();
+    instance.setActivationWindow(&w);
+    int code = instance.exec();
     closeSystemService();
     return code;
 }
